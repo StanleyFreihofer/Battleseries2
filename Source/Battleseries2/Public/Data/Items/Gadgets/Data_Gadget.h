5b1b6e6751f18cf4834fed8847a7e6038e114b3b
Stanley Freihofer <137660221+StanleyFreihofer@users.noreply.github.com>
2026-07-29 23:38:56 -0400

    SMAW  Anim Fix
    -Fixed SMAW Animations (still in testing but results are good). FIXED BY RETARGETING ANIMATIONS!!!!!!!!!!!!!
    -added more gadget static data
    -switched IDE to Rider :D

--- Source/Battleseries2/Public/Data/Items/Gadgets/Data_Gadget.h
+++ Source/Battleseries2/Public/Data/Items/Gadgets/Data_Gadget.h
@@ -2,8 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
-#include "Data/Items/ItemEnums.h"
 #include "Data_Gadget.generated.h"
+
+
+/** 
+ * data FOR CLASSIC GADGETS ONLY
+ * gadgets that are weapons or vehicles in some manner should be listed in their respective data
+**/
 
 USTRUCT(BlueprintType)
 struct FGadgetData : public FTableRowBase
@@ -15,7 +20,22 @@ struct FGadgetData : public FTableRowBase
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FText Description = FText();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ToolTip = "mesh that you hold"))
+	TSoftObjectPtr<UStaticMesh> GadgetMesh = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	int32 MaxActiveInstances = 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, gadgets 'ammo' count will automatically start replenishing"))
+	bool AutoRefill = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, will automatically drop gadget on equip"))
+	bool AutoDrop = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, this gadget is able to be picked back when placed"))
+	bool AbleToPickup = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftClassPtr<AActor> PlacedActorClass = nullptr;
 };
--- Source/Battleseries2/Public/Data/Items/Gadgets/GadgetEnums.h
+++ Source/Battleseries2/Public/Data/Items/Gadgets/GadgetEnums.h
@@ -2,3 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "GadgetEnums.generated.h"
+
+UENUM(BlueprintType)
+enum class EGadgetType : uint8
+{
+	Weapon		UMETA(DisplayName = "Weapon", ToolTip = "Gadget that is a weapon"),
+	Vehicle		UMETA(DisplayName = "Vehicle", ToolTip = "Gadget that is a vehicle (EOD Bot, Drone, etc)"),
+	Gadget		UMETA(DisplayName = "Gadget", ToolTip = "Every other type of gadget")
+};
