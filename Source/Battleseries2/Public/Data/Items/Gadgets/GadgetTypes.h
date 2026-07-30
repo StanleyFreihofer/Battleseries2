045bdb15caf724deca05f4805acae9ce44545048
Stanley Freihofer <137660221+StanleyFreihofer@users.noreply.github.com>
2026-07-30 14:25:48 -0400

    added gadget runtime data

---
+++ Source/Battleseries2/Public/Data/Items/Gadgets/GadgetTypes.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GadgetTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGadgetState
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	FName GadgetID = NAME_None;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (ToolTip = "current number of this gadget at runtime"))
+	int32 CurrentInventory = 0;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TArray<TWeakObjectPtr<AActor>> ActivePlacedInstances;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TWeakObjectPtr<UStaticMeshComponent> HeldMesh_FP = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TWeakObjectPtr<UStaticMeshComponent> HeldMesh_TP = nullptr;
+};
