d02347a4fcb17fb46244892d7a6ff2d4d3372764
Stanley Freihofer <137660221+StanleyFreihofer@users.noreply.github.com>
2026-07-30 12:32:14 -0400

    minor gadget data update

--- Source/Battleseries2/Public/Data/Items/Gadgets/Data_Gadget.h
+++ Source/Battleseries2/Public/Data/Items/Gadgets/Data_Gadget.h
@@ -23,12 +23,6 @@ struct FGadgetData : public FTableRowBase
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ToolTip = "mesh that you hold"))
 	TSoftObjectPtr<UStaticMesh> GadgetMesh = nullptr;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int32 MaxActiveInstances = 1;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, gadgets 'ammo' count will automatically start replenishing"))
-	bool AutoRefill = false;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, will automatically drop gadget on equip"))
 	bool AutoDrop = false;
@@ -36,6 +30,17 @@ struct FGadgetData : public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, this gadget is able to be picked back when placed"))
 	bool AbleToPickup = false;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "the gadget class/object that will be placed in the world"))
 	TSoftClassPtr<AActor> PlacedActorClass = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "the default amount of the gadget a person gets"))					//same as max active instances?		//max inventory count?
+	int32 DefaultInventoryCount = 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (ToolTip = "if true, gadgets 'ammo' count will automatically start replenishing"))
+	bool AutoRefill = false;
+	
+	//replenish delay?
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (TooTip = "the maximum amount of gadget entities/objects that can be placed/active in the world at once. If exceeded the first placed gadget will be deleted and removed from the world"))
+	int32 MaxActiveInstances = 1;
 };
